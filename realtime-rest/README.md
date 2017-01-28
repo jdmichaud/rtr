09e69a501ea3f3da5103ac9baac50f6168ae9d3d
jd <jean.daniel.michaud@gmail.com>
2017-01-28 14:53:12 +0100

    Make it work on node.

--- realtime-rest/README.md
+++ realtime-rest/README.md
@@ -1,17 +1,21 @@
 # Small long-polling client
 
-This tool, provided with the django-realtime-rest, can be used to easily link a model (a simple javascript object) to data fed by the server through the use of a long-polling call.
+This tool, provided with the django-realtime-rest, can be used to easily link a
+model (a simple javascript object) to data fed by the server through the use of
+a long-polling call.
 
 ### Usage
 
-To use it, just create an RTView object and then call monitor:
+To try it in node.js, just create an RTView object and then call monitor:
 ```
-const rtView = RTView('/api/my-models-rt/', function (data) {
-  // Do something with the data, which is a Javascript object
-});
+var realtime_rest = require('realtime_test');
+const rt_view = realtime_rest.RTView('localhost:8000/api/my-models-rt/', function (data) {
+  console.log(data);
+}, realtime_rest.nodeGet);
 ```
 
-If you work with angular, RTView can take a third parameter which will be the function used to perform the HTTP GET call:
+If you work with angular, RTView can take a third parameter which will be the
+function used to perform the HTTP GET call:
 ```
 const rtView = RTView('/api/my-models-rt/', function (data) {
   $scope.evalAsynch(function () {
@@ -19,3 +23,28 @@ const rtView = RTView('/api/my-models-rt/', function (data) {
   });
 }, $http.get);
 ```
+
+### Get function
+
+/!\ Note that by default, `realtime_rest` will use the `XMLHttpRequest` browser
+function to perform the get request. If you use the tool on node, use the node
+compatible function `nodeGet`. You can also provide you own implementation of
+get which shall follow the following specifications:
+
+1. It shall take a url as string
+2. It shall return a ES6 promise which resolve an object containing a data field
+which points to string of data received like:
+```
+{
+  data: '[{"myField": "foo"}]',
+}
+```
+3. It shall reject in case of error with an object like:
+```
+{
+  status: <status code>,
+  message: <error message>,
+}
+```
+
+angular's `$http.get` satisfies those requirements.
--- realtime-rest/index.js
+++ realtime-rest/index.js
@@ -1,22 +1,6 @@
-function embeddedHttpGet(url)
-{
-  return new Promise(function (resolve, reject) {
-    const xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-        resolve({ status: xmlHttp.status, data: xmlHttp.responseText });
-      } else {
-        reject(xmlHttp);
-      }
-    }
-    xmlHttp.open("GET", url, true); // true for asynchronous
-    xmlHttp.send(null);
-  });
-}
-
 function poll(getFn, url, model) {
-  get(url).then((response) => {
-    if (typeof model === "function") {
+  return getFn(url).then((response) => {
+    if (typeof model === 'function') {
       model(JSON.parse(response.data));
     } else {
       model = JSON.parse(response.data);
@@ -24,41 +8,112 @@ function poll(getFn, url, model) {
   });
 }
 
+function RTViewModule() {
+  /**
+   * An embedded get function based on XMLHttpRequest, provided by modern
+   * browsers.
+   * @param  {string}     url
+   * @return {[Promise]}  Promise resolved on server response.
+   */
+  this.browserGet = function browserGet(_url) {
+    return new Promise((resolve, reject) => {
+      const xmlHttp = new XMLHttpRequest();
+      xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+          resolve({ status: xmlHttp.status, data: xmlHttp.responseText });
+        } else {
+          reject(xmlHttp);
+        }
+      };
+      xmlHttp.open('GET', _url, true); // true for asynchronous
+      xmlHttp.send(null);
+    });
+  };
 
-/**
- * Create an object to interact with the -rt url of django-realtime-rest
- * @param {string}             url     the url of the -rt API (usually
- *                                     '/modelname-rt')
- * @param {function or object} model   model is either an object that will be
- *                                     updated with data sent by the server or
- *                                     a function called with the data sent by
- *                                     the server
- * @param {function}           httpGet (optional) A function that takes a url
- *                                     and returns a promise which resolves with
- *                                     the data sent by the server. Could be
- *                                     useful when working with a framework that
- *                                     uses its own http facility (e.g $http)
- */
-function RTView(url, model, httpGet) {
-  const service = {};
-  let running = false;
-  let getFn = (httpGet || embeddedHttpGet);
+  /**
+   * An embedded get function based on node's http module, provided by the
+   * node standard library.
+   * @param  {string}     url
+   * @return {[Promise]}  Promise resolved on server response.
+   */
+  this.nodeGet = function nodeGet(_url) {
+    return new Promise((resolve, reject) => {
+      var http = require('http');
+      const urlModule = require('url');
+      if (!_url.startsWith('http')) _url = `http://${_url}`;
+      _url = urlModule.parse(_url);
+      const req = http.request({
+        hostname: _url.hostname,
+        port: _url.port,
+        path: _url.path,
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }, (res) => {
+        if (res.statusCode !== 200) {
+          reject({ status: res.statusCode, response: `Request Failed.\nStatus Code: ${res.statusCode}` });
+        } else if (!/^application\/json/.test(res.headers['content-type'])) {
+          reject({
+            status: res.statusCode,
+            response: `Invalid content-type.\nExpected application/json but received ${res.headers['content-type']}`,
+          });
+        }
+        let data = '';
+        res.on('data', (chunk) => {
+          data += chunk;
+        });
+        res.on('end', () => {
+          resolve({ data: data });
+        });
+      });
+      req.on('error', (e) => {
+        reject({ status: e.statusCode, response: e.message });
+      });
+      req.end();
+    });
+  };
 
-  service.monitor = function monitor() {
-    running = true;
+  /**
+   * Create an object to interact with the -rt url of django-realtime-rest
+   * @param {string}             url     the url of the -rt API (usually
+   *                                     '/modelname-rt')
+   * @param {function or object} model   model is either an object that will be
+   *                                     updated with data sent by the server or
+   *                                     a function called with the data sent by
+   *                                     the server
+   * @param {function}           httpGet (optional) A function that takes a url
+   *                                     and returns a promise which resolves with
+   *                                     the data sent by the server. Could be
+   *                                     useful when working with a framework that
+   *                                     uses its own http facility (e.g $http)
+   */
+  this.RTView = function RTView(url, model, httpGet) {
+    const service = {};
+    let running = false;
+    const getFn = (httpGet || service.browserGet);
 
-    function _while(condition) {
-      if (condition) {
-        // setTimeout make this a non recursive called to avoid stack overflow.
-        poll(getFn, url, model).then(() => setTimeout(while(running), 0));
+
+    service.monitor = function monitor() {
+      running = true;
+
+      function next(condition) {
+        if (condition) {
+          // setTimeout make this a non recursive called to avoid stack overflow.
+          poll(getFn, url, model)
+            .then(() => setTimeout(next(running), 0))
+            .catch(() => {}); // Silently ignore
+        }
       }
-    }
-    _while(running);
-  }
+      next(running);
+    };
 
-  service.interrupt = function () { running = false; };
+    service.interrupt = function () { running = false; };
 
-  return service;
+    return service;
+  };
+
+  return this;
 }
 
-module.exports = RTView;
+module.exports = RTViewModule();
